2cc3fa1fd322596cc0088331e9ef11a0641fa936
hunzzzzz <hunzz.study@gmail.com>
2024-02-26 20:47:48 +0900

    [Feat] Swagger 관련 라이브러리 추가
    - build.gradle에 Swagger 관련 라이브러리 주입
    - Swagger 관련 설정 파일 생성 (SwaggerConfig)

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,8 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.12.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
+    // SWAGGER
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
     // TEST
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
