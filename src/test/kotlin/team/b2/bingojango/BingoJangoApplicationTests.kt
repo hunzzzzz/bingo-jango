4600d8c3d14b4386bcf35411b033a65ca2a3375c
hunzzzzz <hunzz.study@gmail.com>
2024-02-26 19:52:12 +0900

    [Test] Refrigerator와 Food에 임시 값을 대입하기 위한 테스트 코드 작성
    - Refrigerator와 Food에 임시 값을 대입하기 위한 테스트 코드 작성 (Refrigerator 객체 1개, Food 객체 2개)

--- src/test/kotlin/team/b2/bingojango/BingoJangoApplicationTests.kt
+++ src/test/kotlin/team/b2/bingojango/BingoJangoApplicationTests.kt
@@ -1,13 +1,52 @@
 package team.b2.bingojango
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.repository.findByIdOrNull
+import team.b2.bingojango.domain.food.model.Food
+import team.b2.bingojango.domain.food.model.FoodCategory
+import team.b2.bingojango.domain.food.repository.FoodRepository
+import team.b2.bingojango.domain.refrigerator.model.Refrigerator
+import team.b2.bingojango.domain.refrigerator.repository.RefrigeratorRepository
+import java.time.ZonedDateTime
 
 @SpringBootTest
 class BingoJangoApplicationTests {
+    @Autowired
+    lateinit var foodRepository: FoodRepository
+
+    @Autowired
+    lateinit var refrigeratorRepository: RefrigeratorRepository
 
     @Test
-    fun contextLoads() {
+    fun 냉장고임시등록() {
+        refrigeratorRepository.save(
+            Refrigerator(name = "임시 냉장고")
+        )
+    }
+
+    @Test
+    fun 음식임시등록() {
+        foodRepository.save(
+            Food(
+                category = FoodCategory.FRUIT,
+                name = "사과",
+                expirationDate = ZonedDateTime.now().plusDays(7),
+                purchase = null,
+                refrigerator = refrigeratorRepository.findByIdOrNull(1) ?: throw Exception("")
+            )
+        )
+
+        foodRepository.save(
+            Food(
+                category = FoodCategory.DRINK,
+                name = "우유",
+                expirationDate = ZonedDateTime.now().plusDays(3),
+                purchase = null,
+                refrigerator = refrigeratorRepository.findByIdOrNull(1) ?: throw Exception("")
+            )
+        )
     }
 
 }
