305d580ff41e09084799ee21dbd1393649d788a6
hunzzzzz <hunzz.study@gmail.com>
2024-02-26 19:13:24 +0900

    [Feat] 각 엔티티 별 기본 Controller, Service, Repository 생성
    - 각 엔티티 별 기본 Controller, Service, Repository 생성

--- src/main/kotlin/team/b2/bingojango/domain/refrigerator/repository/RefrigeratorRepository.kt
+++ src/main/kotlin/team/b2/bingojango/domain/refrigerator/repository/RefrigeratorRepository.kt
@@ -1,7 +1,9 @@
 package team.b2.bingojango.domain.refrigerator.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import team.b2.bingojango.domain.refrigerator.model.Refrigerator
 
+@Repository
 interface RefrigeratorRepository : JpaRepository<Refrigerator, Long> {
 }
