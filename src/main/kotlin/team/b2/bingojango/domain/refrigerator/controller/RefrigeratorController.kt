52395a63fac692a7d6d33100dceb3fcbe12a8935
hjm <gks777777@gmail.com>
2024-02-27 20:42:31 +0900

    [Feat] 냉장고 조회, 생성 기능 구현
    - getRefrigerator, addRefrigerator 메서드 추가

--- src/main/kotlin/team/b2/bingojango/domain/refrigerator/controller/RefrigeratorController.kt
+++ src/main/kotlin/team/b2/bingojango/domain/refrigerator/controller/RefrigeratorController.kt
@@ -1,9 +1,38 @@
 package team.b2.bingojango.domain.refrigerator.controller
 
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import io.swagger.v3.oas.annotations.Operation
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.*
+import team.b2.bingojango.domain.refrigerator.dto.AddRefrigeratorRequest
+import team.b2.bingojango.domain.refrigerator.dto.RefrigeratorResponse
+import team.b2.bingojango.domain.refrigerator.service.RefrigeratorService
+import team.b2.bingojango.global.security.UserPrincipal
 
 @RestController
-@RequestMapping("/")
-class RefrigeratorController {
+@RequestMapping("/api/v1/refrigerator")
+class RefrigeratorController(
+    private val refrigeratorService: RefrigeratorService
+) {
+    @Operation(summary = "냉장고 목록 조회")
+    @GetMapping
+    fun getRefrigerator(
+        @AuthenticationPrincipal userPrincipal: UserPrincipal
+    ): ResponseEntity<List<RefrigeratorResponse>> {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(refrigeratorService.getRefrigerator(userPrincipal))
+    }
+
+    @Operation(summary = "냉장고 생성")
+    @PutMapping
+    fun addRefrigerator(
+        @AuthenticationPrincipal userPrincipal: UserPrincipal,
+        @RequestBody request: AddRefrigeratorRequest
+    ): ResponseEntity<RefrigeratorResponse> {
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(refrigeratorService.addRefrigerator(userPrincipal, request))
+    }
 }
---
+++ src/main/kotlin/team/b2/bingojango/domain/refrigerator/dto/AddRefrigeratorRequest.kt
@@ -0,0 +1,6 @@
+package team.b2.bingojango.domain.refrigerator.dto
+
+data class AddRefrigeratorRequest(
+    val name: String,
+    val password: String,
+)
