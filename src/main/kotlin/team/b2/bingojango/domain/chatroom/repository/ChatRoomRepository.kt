5d361e1b11049af888c553679013379d6fd07c57
WiGenie <djg03092@naver.com>
2024-02-28 20:43:22 +0900

    채팅방 기초 세팅 및 websocket 설정

--- src/main/kotlin/team/b2/bingojango/domain/chatroom/repository/ChatRoomRepository.kt
+++ src/main/kotlin/team/b2/bingojango/domain/chatroom/repository/ChatRoomRepository.kt
@@ -2,11 +2,9 @@ package team.b2.bingojango.domain.chatroom.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
 import team.b2.bingojango.domain.chatroom.model.ChatRoom
-import team.b2.bingojango.domain.member.model.Member
 import team.b2.bingojango.domain.refrigerator.model.Refrigerator
 
 interface ChatRoomRepository:JpaRepository<ChatRoom, Long> {
 
-    fun findByRefrigerator(refrigerator: Refrigerator) : ChatRoom
-
+    fun findByRefrigerator(refrigerator: Refrigerator) : ChatRoom?
 }
--- src/main/kotlin/team/b2/bingojango/domain/chatroom/service/ChatRoomService.kt
+++ src/main/kotlin/team/b2/bingojango/domain/chatroom/service/ChatRoomService.kt
@@ -1,15 +1,15 @@
 package team.b2.bingojango.domain.chatroom.service
 
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import team.b2.bingojango.domain.chatroom.model.ChatRoom
 import team.b2.bingojango.domain.chatroom.model.ChatRoomStatus
 import team.b2.bingojango.domain.chatroom.repository.ChatRoomRepository
-import team.b2.bingojango.domain.member.model.Member
+import team.b2.bingojango.domain.member.model.MemberRole
 import team.b2.bingojango.domain.member.repository.MemberRepository
 import team.b2.bingojango.domain.refrigerator.model.Refrigerator
 import team.b2.bingojango.domain.refrigerator.repository.RefrigeratorRepository
 import team.b2.bingojango.domain.user.repository.UserRepository
+import team.b2.bingojango.global.exception.cases.ModelNotFoundException
 import team.b2.bingojango.global.security.UserPrincipal
 
 @Service
@@ -29,17 +29,20 @@ class ChatRoomService(
             )
         )
 
-    fun getChatRoomMember(refrigerator: Refrigerator): List<String>{
-        val members= memberRepository.findAllByRefrigerator(refrigerator)
-        return members.map { "${it.user.name} / ${it.role}\n" }
+    fun getChatRoomMember(refrigerator: Refrigerator): List<String> {
+        val members = memberRepository.findAllByRefrigerator(refrigerator)
+        return members
+            .sortedByDescending { it.role==MemberRole.STAFF }
+            .map { "${it.user.name} / ${it.role}\n" }
     }
 
-    fun deleteChatRoom(refrigerator: Refrigerator){
-        val chatRoom= getChatRoom(refrigerator)
+    fun deleteChatRoom(refrigerator: Refrigerator) {
+        val chatRoom = getChatRoom(refrigerator)
         chatRoom.chatRoomStatus = ChatRoomStatus.DELETED
         chatRoomRepository.save(chatRoom)
     }
 
-    fun getChatRoom(refrigerator: Refrigerator)= chatRoomRepository.findByRefrigerator(refrigerator)
+    fun getChatRoom(refrigerator: Refrigerator) =
+        chatRoomRepository.findByRefrigerator(refrigerator) ?: throw ModelNotFoundException("refrigerator")
 
 }
