f4ddda91ee97a51857ec8ba45a1f94240a7c6c69
qordpsem <qkrrb1223@naver.com>
2024-02-28 11:49:26 +0900

    [Feat] Food 기본 crud
    - FoodController, FoodService에 음식 추가, 삭제, 수정, 수량수정 구현
    - dto 추가

--- src/main/kotlin/team/b2/bingojango/domain/food/controller/FoodController.kt
+++ src/main/kotlin/team/b2/bingojango/domain/food/controller/FoodController.kt
@@ -1,12 +1,12 @@
 package team.b2.bingojango.domain.food.controller
 
 import io.swagger.v3.oas.annotations.Operation
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 import team.b2.bingojango.domain.food.service.FoodService
+import team.b2.bingojango.domain.food.dto.AddFoodRequest
+import team.b2.bingojango.domain.food.dto.UpdateFoodRequest
 
 @RestController
 @RequestMapping("/api/v1/refrigerator/{refrigeratorId}/foods")
@@ -21,4 +21,38 @@ class FoodController(
         @RequestParam count: Int
     ) =
         foodService.addFoodToPurchase(refrigeratorId, foodId, count)
+
+    @PostMapping
+    fun addFood(
+            @PathVariable refrigeratorId: Long,
+            @RequestBody addFoodRequest: AddFoodRequest) : ResponseEntity<Unit> {
+        foodService.addFood(refrigeratorId, addFoodRequest)
+        return ResponseEntity(HttpStatus.CREATED)
+    }
+
+    @PutMapping("/{foodId}")
+    fun updateFood(
+            @PathVariable refrigeratorId: Long,
+            @PathVariable foodId: Long,
+            @RequestBody updateFoodRequest: UpdateFoodRequest): ResponseEntity<Unit> {
+        foodService.updateFood(refrigeratorId, foodId, updateFoodRequest)
+        return ResponseEntity(HttpStatus.OK)
+    }
+
+    @PatchMapping("/{foodId}")
+    fun updateFoodCount(
+            @PathVariable refrigeratorId: Long,
+            @PathVariable foodId: Long,
+            @RequestParam count: Int): ResponseEntity<Unit> {
+        foodService.updateFoodCount(refrigeratorId, foodId, count)
+        return ResponseEntity(HttpStatus.OK)
+    }
+
+    @DeleteMapping("/{foodId}")
+    fun deleteFood(
+            @PathVariable refrigeratorId: Long,
+            @PathVariable foodId: Long): ResponseEntity<Unit> {
+        foodService.deleteFood(refrigeratorId, foodId)
+        return ResponseEntity.noContent().build()
+    }
 }
--- src/main/kotlin/team/b2/bingojango/domain/food/service/FoodService.kt
+++ src/main/kotlin/team/b2/bingojango/domain/food/service/FoodService.kt
@@ -10,11 +10,18 @@ import team.b2.bingojango.domain.purchase.repository.PurchaseRepository
 import team.b2.bingojango.domain.purchase.service.PurchaseService
 import team.b2.bingojango.domain.purchase_product.model.PurchaseProduct
 import team.b2.bingojango.domain.purchase_product.repository.PurchaseProductRepository
+import team.b2.bingojango.domain.food.dto.AddFoodRequest
+import team.b2.bingojango.domain.food.dto.UpdateFoodRequest
+import team.b2.bingojango.domain.food.model.Food
+import team.b2.bingojango.domain.food.model.FoodCategory
+import team.b2.bingojango.domain.refrigerator.repository.RefrigeratorRepository
+import java.time.ZonedDateTime
 
 @Service
 @Transactional
 class FoodService(
     private val foodRepository: FoodRepository,
+    private val refrigeratorRepository: RefrigeratorRepository,
     private val purchaseService: PurchaseService,
     private val purchaseRepository: PurchaseRepository,
     private val purchaseProductRepository: PurchaseProductRepository
@@ -52,4 +59,48 @@ class FoodService(
     // [내부 메서드] id로 Food 객체 가져오기
     private fun getFood(foodId: Long) =
         foodRepository.findByIdOrNull(foodId) ?: throw Exception("") // TODO
+
+
+    @Transactional
+    fun addFood(refrigeratorId: Long, request: AddFoodRequest){
+
+        val findRefrigerator = refrigeratorRepository.findByIdOrNull(refrigeratorId) ?: throw Exception("냉장고를 찾을 수 없습니다.")
+        //해당 냉장고에 추가한 음식이 이미 있으면 이미 존재하는 음식이라고 응답하기?
+        val newFood = Food(
+                category = request.category,
+                name = request.name,
+                expirationDate = ZonedDateTime.now(),
+                count = request.count,
+                refrigerator = findRefrigerator
+        )
+        foodRepository.save(newFood)
+    }
+
+    @Transactional
+    fun updateFood(refrigeratorId: Long, foodId: Long, request: UpdateFoodRequest){
+        val findRefrigerator = refrigeratorRepository.findByIdOrNull(refrigeratorId) ?: throw Exception("냉장고를 찾을 수 없습니다.")
+        val findFood = foodRepository.findByIdOrNull(foodId) ?: throw Exception("음식을 찾을 수 없습니다.")
+        if (findFood.refrigerator?.id != findRefrigerator.id){throw Exception("냉장고에 음식이 존재하지 않습니다.")}
+        findFood.category = FoodCategory.valueOf(request.category)
+        findFood.name = request.name
+        //findFood.expirationDate = request.expirationDate
+        foodRepository.save(findFood)
+    }
+
+    @Transactional
+    fun updateFoodCount(refrigeratorId: Long, foodId: Long, count: Int){
+        val findRefrigerator = refrigeratorRepository.findByIdOrNull(refrigeratorId) ?: throw Exception("냉장고를 찾을 수 없습니다.")
+        val findFood = foodRepository.findByIdOrNull(foodId) ?: throw Exception("음식을 찾을 수 없습니다.")
+        if (findFood.refrigerator?.id != findRefrigerator.id){throw Exception("냉장고에 음식이 존재하지 않습니다.")}
+        findFood.count = count
+        foodRepository.save(findFood)
+    }
+
+    @Transactional
+    fun deleteFood(refrigeratorId: Long, foodId: Long){
+        val findRefrigerator = refrigeratorRepository.findByIdOrNull(refrigeratorId) ?: throw Exception("냉장고를 찾을 수 없습니다.")
+        val findFood = foodRepository.findByIdOrNull(foodId) ?: throw Exception("음식을 찾을 수 없습니다.")
+        if (findFood.refrigerator?.id != findRefrigerator.id){throw Exception("냉장고에 음식이 존재하지 않습니다.")}
+        foodRepository.delete(findFood)
+    }
 }
