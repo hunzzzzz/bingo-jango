94d891b44762294fd3ff65591aef1b7f1b89114c
hunzzzzz <hunzz.study@gmail.com>
2024-02-28 10:06:00 +0900

    [Refactor] ErrorResponse 속성 추가 및 GlobalExceptionHandler에 예외 케이스 추가
    - ErrorResponse httpStatus, path, time 속성 추가
    - GlobalExceptionHandler에 예외 케이스 추가

--- src/main/kotlin/team/b2/bingojango/global/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/team/b2/bingojango/global/exception/GlobalExceptionHandler.kt
@@ -1,20 +1,30 @@
 package team.b2.bingojango.global.exception
 
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import team.b2.bingojango.global.exception.cases.ModelNotFoundException
+import team.b2.bingojango.global.exception.cases.*
 
 @RestControllerAdvice
-class GlobalExceptionHandler {
+class GlobalExceptionHandler(
+    private val httpServletRequest: HttpServletRequest
+) {
 
     // 특정 엔터티 조회 실패
     @ExceptionHandler(ModelNotFoundException::class)
     fun handleModelNotFoundException(e: ModelNotFoundException)
             : ResponseEntity<ErrorResponse> {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-            .body(ErrorResponse(e.message))
+            .body(
+                ErrorResponse(
+                    httpStatus = "404 Not Found",
+                    message = e.message.toString(),
+                    path = httpServletRequest.requestURI
+                )
+            )
     }
 
     // 잘못된 요청
@@ -22,7 +32,64 @@ class GlobalExceptionHandler {
     fun handleModelNotFoundException(e: IllegalArgumentException)
             : ResponseEntity<ErrorResponse> {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-            .body(ErrorResponse(e.message))
+            .body(
+                ErrorResponse(
+                    httpStatus = "400 Bad Request",
+                    message = e.message.toString(),
+                    path = httpServletRequest.requestURI
+                )
+            )
     }
 
+    // role 에 맞지 않는 요청
+    @ExceptionHandler(InvalidRoleException::class)
+    fun handleInvalidRoleException(e: InvalidRoleException) =
+        ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(getErrorResponse(HttpStatus.UNAUTHORIZED, e))
+
+    // 권한 없음
+    @ExceptionHandler(InvalidCredentialException::class)
+    fun handleInvalidRoleException(e: InvalidCredentialException) =
+        ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(getErrorResponse(HttpStatus.UNAUTHORIZED, e))
+
+    // 현재 진행 중인 공동구매 없음
+    @ExceptionHandler(NoCurrentPurchaseException::class)
+    fun handleNoCurrentPurchaseException(e: NoCurrentPurchaseException) =
+        ResponseEntity.badRequest().body(getErrorResponse(HttpStatus.BAD_REQUEST, e))
+
+    // 이미 공동구매 목록 안에 신청하고자 하는 식품이 존재
+    @ExceptionHandler(AlreadyInPurchaseException::class)
+    fun handleAlreadyInPurchaseException(e: AlreadyInPurchaseException) =
+        ResponseEntity.badRequest().body(getErrorResponse(HttpStatus.BAD_REQUEST, e))
+
+    // 중복 투표
+    @ExceptionHandler(DuplicatedVoteException::class)
+    fun handleDuplicatedVoteException(e: DuplicatedVoteException) =
+        ResponseEntity.badRequest().body(getErrorResponse(HttpStatus.BAD_REQUEST, e))
+
+    // Validation 미통과
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleMethodArgumentNotValidException(e: MethodArgumentNotValidException) =
+        ResponseEntity.badRequest().body(
+            ErrorResponse(
+                httpStatus = "400 Bad Request",
+                message = e.bindingResult.allErrors.toMutableList().first().defaultMessage!!,
+                path = httpServletRequest.requestURI.toString()
+            )
+        )
+
+    private fun getErrorResponse(httpStatus: HttpStatus, e: Exception) = when (httpStatus) {
+        HttpStatus.BAD_REQUEST -> ErrorResponse(
+            httpStatus = "400 Bad Request",
+            message = e.message.toString(),
+            path = httpServletRequest.requestURI
+        )
+
+        HttpStatus.UNAUTHORIZED -> ErrorResponse(
+            httpStatus = "401 Unauthorized",
+            message = e.message.toString(),
+            path = httpServletRequest.requestURI
+        )
+
+        else -> null
+    }
 }
