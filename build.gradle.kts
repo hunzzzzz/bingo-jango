fbf8282e9749009f15644c2aa21170ab1ad416c3
hunzzzzz <152062846+hunzzzzz@users.noreply.github.com>
2024-03-08 16:55:12 +0900

    [Refactor] build.gradle에 라이브러리 추가 및 표기 방법 수정
    - 호환성 문제로 인한 kotest, mockk 버젼 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,9 +22,9 @@ repositories {
 
 val awsVersion = "2.2.6.RELEASE"
 val jjwtVersion = "0.12.5"
-val kotestVersion = "5.8.0"
+val kotestVersion = "5.5.5"
 val kotestExtensionVersion = "4.4.3"
-val mockkVersion = "1.3.10"
+val mockkVersion = "1.3.8"
 val processorVersion = "3.2.3"
 val queryDslVersion = "5.0.0"
 val slackVersion = "1.38.1"
