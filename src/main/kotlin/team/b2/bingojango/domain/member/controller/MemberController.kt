7c9b76a533a96dfa0faa1995c81b14752c614d5c
qordpsem <qkrrb1223@naver.com>
2024-03-13 20:47:30 +0900

    [Feat] 냉장고 탈퇴
    - MemberController, MemberService 에 냉장고 탈퇴 기능 추가
    - refrigerator soft delete 만 가능 (DB에서 지우는건 추후 도전..)
    - staff 필수 권한 위임 예외처리로 넘김 (프론트 영역 관련으로 추후 구현..)

--- src/main/kotlin/team/b2/bingojango/domain/member/controller/MemberController.kt
+++ src/main/kotlin/team/b2/bingojango/domain/member/controller/MemberController.kt
@@ -4,28 +4,35 @@ import io.swagger.v3.oas.annotations.Operation
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import team.b2.bingojango.domain.member.service.MemberService
 import team.b2.bingojango.global.security.util.UserPrincipal
 
 @RestController
 @RequestMapping("/api/v1/refrigerator/{refrigeratorId}/members")
-class MemberController (
+class MemberController(
         private val memberService: MemberService
-){
-    // 로그인한 사람이 냉장고 관리자라면 >> 동일 냉장고 타멤버인지 확인하고 >> 관리자 권한주기
+) {
     @Operation(summary = "STAFF 권한 부여")
     @PatchMapping("/{memberId}")
     fun assignStaff(
             @PathVariable refrigeratorId: Long,
             @PathVariable memberId: Long,
             @AuthenticationPrincipal userPrincipal: UserPrincipal
-    ): ResponseEntity<Unit>{
+    ): ResponseEntity<Unit> {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(memberService.assignStaff(refrigeratorId, memberId, userPrincipal))
+    }
+
+    @Operation(summary = "냉장고 탈퇴")
+    @PutMapping
+    fun withdrawMember(
+            @PathVariable refrigeratorId: Long,
+            @AuthenticationPrincipal userPrincipal: UserPrincipal
+    ): ResponseEntity<Unit> {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(memberService.withdrawMember(refrigeratorId, userPrincipal))
     }
 }
