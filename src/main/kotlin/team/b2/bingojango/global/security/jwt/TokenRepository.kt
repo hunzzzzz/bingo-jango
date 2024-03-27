c130cb20da6bb773da2a91dee9d1db3acb7a9986
jeongminy <gks777777@gmail.com>
2024-03-27 18:56:07 +0900

    [Refactor] DeleteToken 수정

--- src/main/kotlin/team/b2/bingojango/global/security/jwt/TokenRepository.kt
+++ src/main/kotlin/team/b2/bingojango/global/security/jwt/TokenRepository.kt
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 import team.b2.bingojango.domain.user.model.User
 
 interface TokenRepository : JpaRepository<RefreshToken, Long> {
-    fun findByUser(user: User): RefreshToken?
+    fun findAllByUser(user: User): List<RefreshToken>?
 }
